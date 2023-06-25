645632cb78aace2bd91fe8298a7cc5c5f9667b5a
Daniela <danielacardona2203@gmail.com>
2023-06-25 23:06:14 +0200

    Implement note addition functionality

--- script.js
+++ script.js
@@ -11,3 +11,24 @@ function getNotes() {
         notesObj = JSON.parse(notes)
     }
 }
+
+//Note btn event listener
+
+noteBtn.addEventListener("click", (e) => {
+    e.preventDefault();
+
+    if (noteTitle.value == "" || noteText.value == "") {
+        return alert("Please add note title and details‼️");
+    }
+
+    getNotes() //notesObj array
+
+    let myObj = {
+        title: noteTitle.value,
+        text: noteText.value,
+    }
+    notesObj.push(myObj)
+    localStorage.setItem("notes", JSON.stringify(notesObj));
+    document.querySelector("form").reset();
+    
+})
